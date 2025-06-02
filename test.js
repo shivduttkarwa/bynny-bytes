c7547339bb3e2a6a3e5e9422bf6b98b4e216fdb9
cybershades-common <shivduttkarwa@gmail.com>
2025-06-02 17:36:27 +0530

    changes

--- test.js
+++
@@ -1,9 +0,0 @@
-// Basic interaction placeholder
-document.addEventListener("DOMContentLoaded", () => {
-  console.log("Bunny Bytes website loaded successfully!");
-
-  const exploreBtn = document.querySelector(".btn");
-  exploreBtn.addEventListener("click", () => {
-    console.log("Explore More clicked");
-  });
-});
